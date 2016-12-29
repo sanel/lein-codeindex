b43d400ccd22bd9fa60db53106cdc203a7d76f28
Sanel Zukan <sanelz@gmail.com>
2016-12-29 01:36:52 +0100

    Marking release.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject codeindex "0.1.0-SNAPSHOT"
-  :description "Index your code with (c|e)tags"
+(defproject lein-codeindex "0.1.0"
+  :description "Index code in this project and all dependencies using etags or ctags."
   :url "https://github.com/sanel/lein-codeindex"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
